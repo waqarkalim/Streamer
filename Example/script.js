85185ae9bdfa7247b13b6755bed611d72b0bef88
Waqar Bin Kalim <waqaar.199@gmail.com>
2018-08-13 20:17:19 -0400

    Changed index.html and added bootstrap

--- Example/script.js
+++ Example/script.js
@@ -1,24 +1,37 @@
 
 output = "";
-
 $.getJSON("JSONData.json", function(data) {
+    output += "<div id='menu-overlay'></div>";
+    output += "<div id='menu'>";
+
+    output += "<ul class='list-group' id='myList'>";
     $.each(data, function(key, val){
         var name = val.Name;
         var season = val.Season;
 
-        output += "<div class='values'>";
-        output += '<h5 class="value-id">' + name + '</h5>';
 
+        output += "<li class='parent list-group-item'><b>" + name + "</b>";
+        output += "<ul class='sub-nav'>";
 
-        $.each(season, function() {
+        $.each(season, function(key, value) {
 
-            output += '<p>' + this.episodeId + '</p>';
+            var seasonObj = this;
+            output += "<li>" + key ;
+            output += "<ul>";
+            $.each(seasonObj, function(key, value) {
+                output += '<li>Episode ' + this.episodeId + '</li>';
+            });
+            output += "</ul>";
+            output += "</li>";
+
         });
 
+        output += "</ul>";
+        output += "</li>";
 
-        output += '<p class="value-name">' + season + '</p>';
-        output += "</div>";
     });
+    output += "</ul>";
+    output += "</div>";
 });
 
 alert(output);
@@ -28,13 +41,28 @@ $(document).ready(function(){
 
 /* SEEKER FUNCTION */
 
-jQuery(function(){
-  var $rows = $('.values');
-  $('#seeker').keyup(function () {
-    var regex =  new RegExp(RegExp.escape($.trim(this.value).replace(/\s+/g, ' ')), 'i')
-    $rows.hide().filter(function () {
-      var text = $(this).children(".value-name").text().replace(/\s+/g, ' ');
-      return regex.test(text)
-    }).show();
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myList li").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+  $('.parent').on("click",function(){
+
+    $(this).find(".sub-nav").toggle();
+    $(this).siblings().find(".sub-nav").hide();
+
+    if($(".sub-nav:visible").length === 0 ){
+      $("#menu-overlay").hide();
+    }else {
+      $("#menu-overlay").show();
+    }
   });
+
+   $("#menu-overlay").on("click",function(){
+     $(".sub-nav").hide();
+     $(this).hide();
+   });
 });
