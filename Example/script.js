aaf3761d7b9198d2c16bff5404fb9a14b078928c
Waqar Bin Kalim <waqaar.199@gmail.com>
2018-08-16 00:04:47 -0400

    Updated json file

--- Example/script.js
+++ Example/script.js
@@ -132,8 +132,9 @@ $(document).ready(function(){
 
     $("#myInput").on("keyup", function() {
         var value = $(this).val().toLowerCase();
-        $("#myList li").filter(function() {
+        $("#myList div").filter(function() {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+
         });
     });
 
